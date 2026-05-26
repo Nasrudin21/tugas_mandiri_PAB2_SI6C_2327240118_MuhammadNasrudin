108ac3cf994ddc8c4395928dd9fd76790fc777a2
Nasrudin <muhammadnasrudin674@gmail.com>
2026-05-26 12:48:36 +0700

    update Traffil list

--- monitoring_kemacetan/lib/widgets/traffic_list_item.dart
+++ monitoring_kemacetan/lib/widgets/traffic_list_item.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:monitoring_kemacetan/models/traffic.dart';
 import 'package:monitoring_kemacetan/services/traffic_services.dart';
-
+import 'package:monitoring_kemacetan/screens/detail_screen.dart';
 
 class TrafficListItem
     extends StatelessWidget {
@@ -68,6 +68,94 @@ class TrafficListItem
       shadowColor: Colors.black12,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(24),
+      ),
+
+      child: ListTile(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder:
+                  (_) => DetailScreen(
+                    traffic: traffic,
+                  ),
+            ),
+          );
+        },
+
+        leading:
+            traffic.image != null
+                ? ClipRRect(
+                  borderRadius:
+                      BorderRadius.circular(
+                        8,
+                      ),
+
+                  child: Image.memory(
+                    base64Decode(
+                      traffic.image!,
+                    ),
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                )
+                : const Icon(
+                  Icons.image,
+                  size: 50,
+                ),
+
+        title: Text(
+          traffic.category ??
+              'No Category',
+          style: const TextStyle(
+            fontWeight:
+                FontWeight.bold,
+          ),
+        ),
+
+        subtitle: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.start,
+
+          children: [
+            Text(
+              traffic.description ??
+                  '',
+              maxLines: 2,
+              overflow:
+                  TextOverflow.ellipsis,
+            ),
+
+            const SizedBox(height: 4),
+
+            Text(
+              traffic.userFullName ??
+                  '',
+              style: const TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+
+        isThreeLine: true,
+
+        trailing:
+            isOwner
+                ? IconButton(
+                  onPressed:
+                      () =>
+                          _deleteTraffic(
+                            context,
+                          ),
+
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                )
+                : null,
       ),
     );
   }
